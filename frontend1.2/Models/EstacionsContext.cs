e86a62f5045bfc861175e6894d2159cf118dc4e3
Sergi <saaroca@gmail.com>
2020-12-03 09:54:34 +0100

    ok

---
+++ frontend1.2/Models/EstacionsContext.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication7.Models;
+
+namespace AppMeteo.Models
+{
+    public class EstacionsContext : DbContext
+    {
+
+        public DbSet<Estacions> Estacio { get; set; }
+
+        public EstacionsContext(DbContextOptions<EstacionsContext> options)
+            : base(options)
+        {
+
+        }
+
+
+    }
+}
